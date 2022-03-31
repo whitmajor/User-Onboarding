e14990b54f981edf47f6edaa3c6fb50bbafcbb15
whitmajor <98010797+whitmajor@users.noreply.github.com>
2022-03-30 23:08:35 -0400

    first attempt

---
+++ src/App.js
@@ -0,0 +1,69 @@
+
+import './App.css';
+import React,{useState  } from "react"
+import Form from "./Form"
+ import schema from "./Validation/FormSchema"
+ import * as yup from "yup"
+import axios from "axios"
+
+const initialFormValues ={
+  username: '',
+  password: '',
+  email: '',
+  checked: false,
+}
+const initialFormErrors ={
+  username: '',
+  password: '',
+  email: '',
+  checked: false,
+}
+
+function App() {
+  const [formValues,setFormValues] = useState(initialFormValues)
+  const [formErrors,setFormErrors] = useState(initialFormErrors)
+  const [users, setUsers] = useState([])
+
+const handleSubmit =() =>{
+axios.post("https://reqres.in/api/users",formValues)
+.then(res=>{
+  setUsers([res.data,...users])
+})
+.catch(err=>console.error(err))
+.finally(()=> setFormValues(initialFormValues))
+}
+const validate = (name,value) =>{
+  yup.reach(schema,name)
+  .validate(value)
+  .then(()=>setFormErrors({...formErrors,[name]:" "}))
+  .catch(err => setFormErrors({...formErrors,[name]: err.errors[0]}))
+}
+
+
+  const handleChange = (name,value) =>{
+    validate(name,value)
+    setFormValues({...formValues,[name]: value});
+  }
+ 
+
+  return (
+    <div className="App">
+      <Form
+    values={formValues}
+    change={handleChange}
+    errors = {formErrors}
+     submit={handleSubmit}
+
+   
+    />
+    {users.map(user=> (
+      <div key = {user.id}>
+        <p>{user.createdAt}</p>
+        <p>{user.email}</p>
+        </div>
+    ))}
+    </div>
+  );
+}
+
+export default App;
